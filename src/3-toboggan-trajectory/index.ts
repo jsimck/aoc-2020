812bbfef2d2cab7e134e8049561f97014c3af742
Jan Šimeček <simecekjann@gmail.com>
2021-01-08 08:51:15 +0100

    Added challenge 3 bonus

--- src/3-toboggan-trajectory/index.ts
+++ src/3-toboggan-trajectory/index.ts
@@ -1,36 +1,74 @@
 import chalk from 'chalk';
 import { loadData } from '../utils';
 
-const DIR_X = 3;
-const DIR_Y = 1;
 const TREE_SYMBOL = '#';
+
+type Direction = {
+  x: number;
+  y: number;
+};
 
 function traverse(
   input: string[],
   x: number,
+  dirX: number,
   y: number,
+  dirY: number,
   trees: number = 0
 ): number {
-  if (!input.length || !input[y] || !input[y][x]) {
+  if (!input.length) {
     return trees;
   }
 
-  const newY = y + DIR_Y;
-  const newX = (x + DIR_X) % input[y].length;
-  const hasTree = input[y][x] === TREE_SYMBOL;
+  const newY = y + dirY;
+  const newX = (x + dirX) % input[y].length;
 
-  return traverse(input, newX, newY, hasTree ? trees + 1 : trees);
+  if (!input[newY] || !input[newY][newX]) {
+    return trees;
+  }
+
+  return traverse(
+    input,
+    newX,
+    dirX,
+    newY,
+    dirY,
+    input[newY][newX] === TREE_SYMBOL ? trees + 1 : trees
+  );
 }
 
 export default function (): void {
   const input = loadData(__dirname, './data/input.txt');
-  const numTrees = traverse(input, 0, 0, 0);
+  const numTrees = traverse(input, 0, 3, 0, 1, 0);
 
   console.log(
     chalk.cyan(
       `On your journey down the forest you've encountered ${chalk.bold.underline(
         numTrees
-      )} trees`
+      )} trees.`
+    )
+  );
+
+  // BONUS
+  const directions: Direction[] = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+  ];
+
+  const numTreeMultiplePasses = directions.reduce((acc, cur) => {
+    const path = traverse(input, 0, cur.x, 0, cur.y, 0);
+    console.log(path);
+    return acc * path;
+  }, 1);
+
+  console.log(
+    chalk.cyan(
+      `On your journey down the forest you've encountered ${chalk.bold.underline(
+        numTreeMultiplePasses
+      )} trees.`
     )
   );
 }
